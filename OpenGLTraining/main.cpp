b294f730a660b755cbaee82b816a4fcbf900f1b2
Batname <dadubinin@gmail.comm>
2017-02-18 14:32:43 +0100

    Initial Commit

---
+++ OpenGLTraining/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTraining
+//
+//  Created by bat on 2/18/17.
+//  Copyright © 2017 batlab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
